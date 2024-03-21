5b07b4e90e1c9033aa36765cd0e3af1b38e82f17
Chaitanya Sharma <39400946+CheeksTheGeek@users.noreply.github.com>
2024-03-20 22:12:30 -0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup
 
 setup(
-    name="pyjsoncanvas",
-    version="0.1.0",
+    name="PyJSONCanvas",
+    version="1.0",
     description="A simple library for working with JSON Canvas (previously known as Obsidian Canvas) files.",
     author="Chaitanya Sharma",
-    url="https://github.com/cheeksthegeek/pyjsoncanvas",
+    url="https://github.com/CheeksTheGeek/PyJSONCanvas",
     license="MIT",
     py_modules=["pyjsoncanvas"],
 )
