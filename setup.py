541551e03b492045b99d9f956933fe00256e531a
Chaitanya Sharma <39400946+CheeksTheGeek@users.noreply.github.com>
2024-03-20 22:27:54 -0400

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 
 setup(
     name="PyJSONCanvas",
-    version="1.0",
+    version="1.0.1",
     description="A simple library for working with JSON Canvas (previously known as Obsidian Canvas) files.",
     author="Chaitanya Sharma",
     url="https://github.com/CheeksTheGeek/PyJSONCanvas",
     license="MIT",
     py_modules=["pyjsoncanvas"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
